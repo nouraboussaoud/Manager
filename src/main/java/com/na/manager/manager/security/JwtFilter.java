9cd27381515cbd0dec0017c74689bbcc5d34e164
nouraboussaoud <nour.aboussaoud@esprit.tn>
2025-07-12 00:00:09 +0100

    security config

---
+++ src/main/java/com/na/manager/manager/security/JwtFilter.java
@@ -0,0 +1,7 @@
+package com.na.manager.manager.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtFilter {
+}
--- src/main/java/com/na/manager/manager/user/User.java
+++ src/main/java/com/na/manager/manager/user/User.java
@@ -1,50 +1,98 @@
 package com.na.manager.manager.user;
 
+import com.na.manager.manager.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_user")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails , Principal {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthdate;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+    @CreatedDate
+    @Column(nullable = false , updatable = false)
+    private LocalDate createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDate lastModifiedDate;
+
 
     @Override
     public String getName() {
-        return null;
+        return email;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.roles
+                .stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return !accountLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return enabled;
+    }
+
+    private String fullName() {
+        return firstname + " " + lastname;
     }
 }
